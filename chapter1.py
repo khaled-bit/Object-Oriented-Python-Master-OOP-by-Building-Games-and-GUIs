3f46beb92d7cddfae8641af90184b210fdf8bf86
khaled-bit <81579155+khaled-bit@users.noreply.github.com>
2022-07-27 22:02:03 +0200

    m

--- chapter1.py
+++ chapter1.py
@@ -1,15 +1,16 @@
 import random
 from re import L
-# Card constants
+# suit tuple , rank tuple that will be used in building the dict
 SUIT_TUPLE = ('Spades', 'Hearts', 'Clubs', 'Diamonds')
 RANK_TUPLE = ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack',
 'Queen', 'King')
+#used in the for loop for deciding how many time s to play for the card
 NCARDS = 8
-
+#a function to fin the top of the dic
 def getCard(deckListIn):
     thisCard = deckListIn.pop()
     return thisCard
-
+#a function to suffle (change the order) of the dic
 def shuffle(deckListIn):
     deckListOut = deckListIn.copy()
     random.shuffle(deckListOut)
@@ -21,6 +22,7 @@ print('Getting it right adds 20 points; get it wrong and you lose 15 points.')
 print('You have 50 points to start.')
 print()     
 
+#(0,'a') , (1,'b')
 startingDeckList = []
 for suit in SUIT_TUPLE:
     for thisValue, rank in enumerate(RANK_TUPLE):
